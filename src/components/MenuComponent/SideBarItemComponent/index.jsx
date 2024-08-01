04c047e3ca058f7362e4548f52dc5ef539cf7a7a
NamAst01 <145591149+NamAst01@users.noreply.github.com>
2024-08-01 10:10:57 +0700

    Update SideBarItemComponent

--- src/components/MenuComponent/SideBarItemComponent/index.jsx
+++ src/components/MenuComponent/SideBarItemComponent/index.jsx
@@ -1,67 +1,42 @@
-import {NavLink, useMatch, useResolvedPath} from "react-router-dom";
+import {useEffect, useState} from 'react';
+import {NavLink, useMatch, useResolvedPath} from 'react-router-dom';
 
 const SideBarItemComponent = (props) => {
-    const { to, name, icon, child } = props.item;
+    const {to, name, icon, child = []} = props.item;
+    const [isOpen, setIsOpen] = useState(false);
     const resolved = useResolvedPath(to);
-    const isActive = useMatch({ path: resolved.pathname, end: true });
-    // const {to, name, icon, child = []} = props.item;
-    // const [isOpen, setIsOpen] = useState(false);
-    // const resolved = useResolvedPath(to);
-    // const isActive = useMatch({path: resolved.pathname, end: true});
-    //
-    // useEffect(() => {
-    //     setIsOpen(isActive)
-    // }, []);
+    const isActive = useMatch({path: resolved.pathname, end: true});
+
+    useEffect(() => {
+        setIsOpen(isActive);
+    }, [isActive]);
+
+    const handleToggle = (event) => {
+        setIsOpen(prevIsOpen => !prevIsOpen);
+    };
 
     return (
-        <li className="nav-item menu-open">
-            {child.length > 0 ? (
-                <>
-                    <NavLink to={to} className={isActive ? "nav-link active" : "nav-link"}>
-                        <i className={icon}></i>
-                        <p>
-                            {name}
-                            <i className="fas fa-angle-left right"></i>
-                        </p>
-                    </NavLink>
-                    <ul className="nav nav-treeview">
-                        {child.map((item, index) => (
-                            <SideBarItemComponent item={item} key={index} />
-                        ))}
-                    </ul>
-                </>
-            ) : (
-                <NavLink to={to} className={isActive ? "nav-link active" : "nav-link"}>
-                    <i className={icon}></i>
-                    <p>{name}</p>
-                </NavLink>
+        <li className={`nav-item ${child.length > 0 ? 'menu-open' : ''}`}>
+            <NavLink
+                to={to}
+                className={isActive ? "nav-link active" : "nav-link"}
+                onClick={handleToggle}
+            >
+                <i className={icon}></i>
+                <p>
+                    {name}
+                    {child.length > 0 && (
+                        <i className={`fas fa-angle-${isOpen ? 'down' : 'left'} right`}></i>
+                    )}
+                </p>
+            </NavLink>
+            {child.length > 0 && (
+                <ul className={`nav nav-treeview ${isOpen ? 'd-block' : 'd-none'}`}>
+                    {child.map((item, i) => (
+                        <SideBarItemComponent item={item} key={i}/>
+                    ))}
+                </ul>
             )}
-{/*    const handleToggle = (event) => {*/}
-{/*        setIsOpen(prevIsOpen => !prevIsOpen);*/}
-{/*    };*/}
-
-{/*    return (*/}
-{/*        <li className={`nav-item ${child.length > 0 ? 'menu-open' : ''}`}>*/}
-{/*            <NavLink*/}
-{/*                to={to}*/}
-{/*                className={isActive ? "nav-link active" : "nav-link"}*/}
-{/*                onClick={handleToggle}*/}
-{/*            >*/}
-{/*                <i className={icon}></i>*/}
-{/*                <p>*/}
-{/*                    {name}*/}
-{/*                    {child.length > 0 && (*/}
-{/*                        <i className={`fas fa-angle-${isOpen ? 'down' : 'left'} right`}></i>*/}
-{/*                    )}*/}
-{/*                </p>*/}
-{/*            </NavLink>*/}
-{/*            {child.length > 0 && (*/}
-{/*                <ul className={`nav nav-treeview ${isOpen ? 'd-block' : 'd-none'}`}>*/}
-{/*                    {child.map((item, i) => (*/}
-{/*                        <SideBarItemComponent item={item} key={i}/>*/}
-{/*                    ))}*/}
-{/*                </ul>*/}
-{/*            )}*/}
         </li>
     );
 };
